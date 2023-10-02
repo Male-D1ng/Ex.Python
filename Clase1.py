ee001a37b04414374358961886e38040d4182e6d
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2023-10-02 12:30:37 -0300

    Update Clase1.py

--- Clase1.py
+++ Clase1.py
@@ -1,44 +1,109 @@
-{#Programa Python
-Ejemplo
-problema suma2(x : Z, y : Z) : Z{
-asegura: res = x + y
-}
-def suma2 (x: int, y: int) -> int:
-res: int = x + y
-return res 
+{#Programa Python Ejemplo 
+ # problema suma2(x : Z, y : Z) : Z{
+  #  asegura: res = x + y
+  #}
+ # def suma2 (x: int, y: int) -> int:
+  #res: int = x + y
+  #return res  
 #} 
 
-{--ASIGNACIÓN : Es la operaci´on fundamental para modificar el valor de una variable.
-▶ Sintaxis: variable = expresi´on;
-▶ Es una operaci´on asim´etrica
-▶ Lado izquierdo: debe ir una variable u otra expresi´on que represente
-una posici´on de memoria.
-▶ Lado derecho: una expresi´on del mismo tipo que la variable
-▶ constante,
-▶ variable o
-▶ funci´on aplicada a argumentos.
-▶ Efecto de la asignaci´on:
-1. Se eval´ua la expresi´on de la derecha y se obiene un valor.
-2. Ese valor se copia en el espacio de memoria de la variable.
-3. El resto de la memoria no cambia.
---}
+##para ejecutar python : python3 -> import archivo -> test.funcion(), para salir es Ctrl-d 
+## tengo q asignarle un tipo al dato: los de entrada van entre () y el de salida va por afuera dps de ->
+  
+def prueba (): 
+    print ("hola")
 
-{--Instrucción Return :Termina la ejecuci´on de una funci´on.
-▶ Retorna el control a su invocador.
-▶ Devuelve el valor de la expresi´on como resultado.
-problema suma2(x : Z, y : Z) : Z{
-asegura: res = x + y
-}
-def suma2 (x: int, y: int) -> int:
-res: int = x + y
-return res
-def suma2 (x: int, y: int) -> int:
-return x + y 
---}
+prueba ()
+
+def imprimir_holas ():
+    print ('holas') 
 
 
---para ejecutar python : python3 -> import archivo -> test.funcion(), para salir es Ctrl-d --
+def es_multiplo_de (n:int,m:int)-> bool:
+    res: bool = (n % m) == 0  #significa mod 
+    return res 
 
+def es_nombre_largo (nombre:str) -> bool :
+    res:bool = 3 <= len(nombre) and len(nombre)<= 8 
+    return res 
+
+## el && se escribe and y en la consola los string (str) van con doble comilla
+
+def devolver_el_doble_si_par (par:int) -> int:
+    if es_multiplo_de (par,2) :
+        res: int = 2*par 
+    else:
+        res: int = par
+    return res 
+
+def imprimir_pares ():
+    i : int = 10
+    while i <= 40 :
+        print (i)
+        i = i + 2
+
+## otra forma de escribirlo
+def imprimir_pares ():
+    i : int = 10
+    while i <= 40 :
+        if es_multiplo_de (i,2): 
+            print (i)
+        i = i + 1
+def imprimir_pares_for ():
+    for i in range(10,41,2) # sea range (i,n,j), devuelve hasta el j-1 
+        print(i) 
+
+def cancion (): # el '\n' significa q respeta los saltos de linea de los versos
+    print ("I'am'\n'taking'\n'a'\n'ride'\n'with'\n'my'\n'best'\n'friend")
+
+def raizDe2 (): #primero hay q hacer un import math para usar la raiz. round redondea a 4 digitos en este caso
+    print (round(math.sqrt(2), 4) )
+
+def factorial (f:int) -> int: 
+    res : int = 1
+    for i in range (1,f+1):
+       res = res * i 
+       print (res)
+    return res 
+
+def factorial_while (n:int) -> int :
+    res:int = n
+    i:int = n-1
+    while i>0:
+        res = res*i
+        i = i-1
+        print(res)
+    return res 
+
+def factorial2 (i:int) -> int:
+    res:int = factorial (i) * 2
+
+
+#perimetro: que devuelva el per´ımetro de la circunferencia de radio 1.
+def perimetro ():
+    print (2*math.pi) 
+
+# esta mal, hago q r se divide x si mismo, por eso me da 1.0
+def raiz_cuadrada (r :int) -> int:
+    i:int = r  
+    if es_multiplo_de (r,i):
+        res : int = r/i
+    else: 
+        res :int = r
+    return res 
+
+
+def factorial_de2 (f:int)-> int :
+    res : int = 2*factorial (f)
+    return res 
+  
+## otro de raiz, esta mal pero ya me acerco a la correcta especificacio
+def raiz_cuadrada (r :int) -> int:
+    if es_multiplo_de (r,2): 
+        res : int = int (r/2)
+    else:
+        res : int = int (r/3) # el int me da la parte entera de esa div
+    return res 
 
 
 
