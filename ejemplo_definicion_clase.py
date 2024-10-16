bcfa6d16b878dec82136566870c96364167ab130
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2024-10-16 00:14:02 -0300

    Delete ejemplo_definicion_clase.py

--- ejemplo_definicion_clase.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self, nombre: str, edad:int):
-        self.nombre = nombre
-        self.edad = edad
-        
-    def presentarse(self):
-        print(f"Hola, mi nombre es {self.nombre} y mi edad es {self.edad}")
-        
-
