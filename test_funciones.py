85fbcf65788b24d4f8795467856e814697dfd477
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2024-11-11 13:32:13 -0300

    Update test_funciones.py

--- test_funciones.py
+++ test_funciones.py
@@ -15,6 +15,30 @@ class FuncionesTest(unittest.TestCase):
     def test_3(self):
         self.assertEqual(triangle(1,1,-1), 4, "cond 1 - alguno <0")
 
+    def test_4(self):
+        self.assertEqual(triangle(1,1,1), 1, "triangulo equilatero")
+
+    def test_5(self):
+        self.assertEqual(triangle(5,4,2), 3, "triangulo escaleno")
+
+    def test_6(self):
+        self.assertEqual(triangle(5,5,5), 1, "triangulo equilatero")
+
+    def test_7(self):
+        self.assertEqual(triangle(2,1,4), 4, "invalido")
+
+    def test_8(self):
+        self.assertEqual(triangle(4,-9,1), 4, "invalido negativo")
+
+    def test_9(self):
+        self.assertEqual(triangle(1,6,6), 2, "isosceles")
+
+    def test_10(self):
+        self.assertEqual(triangle(7,10,12), 3, "escaleno")
+
+    def test_11(self):
+        self.assertEqual(triangle(8,5,14), 4, "invalido")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
 
