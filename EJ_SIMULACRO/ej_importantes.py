c05a99e1c2ebe572f9e482eb935aac879066d62a
Male-D1ng <102553397+Male-D1ng@users.noreply.github.com>
2024-11-22 17:38:30 -0300

    Update ej_importantes.py

--- EJ_SIMULACRO/ej_importantes.py
+++ EJ_SIMULACRO/ej_importantes.py
@@ -6,6 +6,7 @@ def columna_n (matriz:list[list[int]],n:int)->list[int]:
     columna:list[int]=[]
     n:int
     for filas in range(len(matriz)):
+        #me interesa que vea todas las filas para que complete la columna
         columna.append(matriz[filas][n])
     return columna
 
@@ -15,7 +16,8 @@ print("columna n=",columna_n(l,n))
 
 def todas_las_columnas(matriz:list[list[int]])->list[list[int]]:
     res:list[list[int]]=[]
-    for filas in range(len(matriz)):
+    for filas in range(len(matriz[0])):
+        #estoy viendo la cantidad de columnas de la primera fila, el resto de las filas tiene igual longitud
         colum_n:list[int] = columna_n(matriz,filas)
         res.append(colum_n)
     return res
@@ -23,6 +25,11 @@ def todas_las_columnas(matriz:list[list[int]])->list[list[int]]:
 l =[[2,3,5],[4,6,23],[5,8,3]]
 print("columna n=",todas_las_columnas(l))
 
-
+m = [[1,3,1,3],
+     [-5,6,-5,6],
+     [6,1,6,1],
+     [1,3,1,3],
+     [-5,6,-5,6],
+     [6,1,6,1]]
 
 
